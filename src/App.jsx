518219abec893ca96300536f675f029e67635f3b
McKenney Leavell <maleavell@gmail.com>
2025-08-15 16:02:02 -0500

    working on adding joke input to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
 import "./App.css"
+import { useState, useEffect } from "react"
+import { getAllJokes, setJokeText } from "./jokeService.jsx";
 
 export const App = () => {
-  return <div>Hello World!</div>
+  const [allJokes, setAllJokes] = useState([]);
+  const [jokeInput, setJokeInput] = useState("");
+
+  useEffect(() => {
+    getAllJokes().then((jokesArray) => {
+        setAllJokes(jokesArray);
+        console.log("jokes set!")
+    })
+  }, [])
+
+
+  return (
+  <div className="jokes-container">
+    <h2>Hello World!</h2>
+    <input
+    className="input-box"
+    type="text"
+    placeholder="New One Liner"
+    onChange={(event) => {
+      // What's the value of event?
+      setJokeInput(event.target.value)
+    }}
+    />
+    <button className="joke-input-submit" onClick={() =>
+        setJokeText(jokeInput)
+    }>Add</button>
+    </div>)
 }
+
