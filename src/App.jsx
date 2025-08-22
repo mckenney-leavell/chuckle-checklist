37f1abb5583e459bbb7f5ecceb33762d4379c5fe
McKenney Leavell <maleavell@gmail.com>
2025-08-22 14:24:21 -0500

    cleaned up process for joke input save

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useState, useEffect } from "react"
 import { getAllJokes } from "./services/jokeService.jsx";
 import { handleAddJoke } from "./JokeButton.jsx";
-// import { transientState } from "./TransientState.jsx"
+import { transientState } from "./TransientState.jsx"
   
 
 
@@ -11,12 +11,26 @@ export const App = () => {
   const [allJokes, setAllJokes] = useState([]);
   const [jokeInput, setJokeInput] = useState("");
 
-  useEffect(() => {
-    getAllJokes().then((jokesArray) => {
-        setAllJokes(jokesArray);
-        console.log("jokes set!")
-    })
-  }, [])
+  // useEffect(() => {
+  //   getAllJokes().then((jokesArray) => {
+  //       setAllJokes(jokesArray);
+  //       console.log("jokes set!")
+  //   })
+  // }, [])
+
+  useEffect (() => {
+    if (jokeInput.length != 0) {
+      const postJoke = {
+        method: "POST",
+        headers: {
+                  "Content-Type": "application/json"
+              },
+        body: JSON.stringify(setJokeInput(jokeInput))
+      }
+      fetch("http://localhost:8088/jokes", postJoke)
+    }
+}, [jokeInput])
+
 
 
   return (
@@ -26,7 +40,7 @@ export const App = () => {
     className="input-box"
     type="text"
     placeholder="New One Liner"
-    value={jokeInput}
+    // value={transientState.text}
     onChange={(event) => {
       // What's the value of event?
       setJokeInput(event.target.value)
--- src/JokeButton.jsx
+++ src/JokeButton.jsx
@@ -1,4 +1,24 @@
-import { postJokes } from "./TransientState.jsx";
+// import { postJokes } from "./TransientState.jsx";
+
+export const postJokes = async (jokeInput) => {
+    if (jokeInput.length != 0) {
+        // transientState.text = jokeInput
+        const jokeData = {
+            text: jokeInput,
+            told: false
+        }
+        const postInput = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(jokeData)
+        }
+        const response = await fetch("http://localhost:8088/jokes", postInput)    
+    console.log(jokeData)
+    }
+
+}
 
 export const handleAddJoke = async (jokeInput) => {
         const jokes = await postJokes(jokeInput)
