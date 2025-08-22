c9cefe6608492403fb430e69a3824da57ac56384
McKenney Leavell <maleavell@gmail.com>
2025-08-22 16:13:08 -0500

    adding css class names

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import "./App.css"
-import React from "react";
 import { useState, useEffect } from "react"
-import { getAllJokes } from "./services/jokeService.jsx";
+// import { getAllJokes } from "./services/jokeService.jsx";
 import { handleAddJoke } from "./JokeButton.jsx";
-  
+import stevePic from "./assets/steve.png"
 
 
 export const App = () => {
@@ -18,22 +17,32 @@ export const App = () => {
   // }, [])
 
   return (
-  <div className="jokes-container">
-    <h2>Hello World!</h2>
-    <input
-    className="input-box"
-    type="text"
-    placeholder="New One Liner"
-    value={jokeInput}
-    onChange={(event) => {
-      // What's the value of event?
-      setJokeInput(event.target.value)
-    }}
-    />
-    <button className="add-joke" onClick={() => {
-        handleAddJoke(jokeInput)
-        setJokeInput("")
-    }}>Add</button>
-    </div>)
+  <div className="app-container">
+    <header className="app-heading">
+      <div className="app-heading-circle">
+        <img className="app-logo" src={stevePic} alt="Good job Steve" />
+      </div>
+      <div className="app-heading">
+        <h1 className="app-heading-text">Chuckle Checklist</h1>
+      </div>
+    </header>
+    <h2>Add joke</h2>
+    <div className="joke-add-form">
+      <input
+      className="joke-input"
+      type="text"
+      placeholder="New One Liner"
+      value={jokeInput}
+      onChange={(event) => {
+        // What's the value of event?
+        setJokeInput(event.target.value)
+      }}
+      />
+      <button className="joke-input-submit" onClick={() => {
+          handleAddJoke(jokeInput)
+          setJokeInput("")
+      }}>Add</button>
+    </div>
+  </div>)
 }
 
